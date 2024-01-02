a46758bbf35e886f4e607df31b6ec4790db0d3fa
Jeffleyg <jefleygarcon007@gmail.com>
2024-01-02 17:06:38 -0300

    tratamento de exception

--- Palindromo/src/application/Program.java
+++ Palindromo/src/application/Program.java
@@ -8,22 +8,27 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite um frase ou uma palavra para verificar seéum palindromo ou não (Vamos lá): ");
-		String input = sc.nextLine();
+		try{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Digite um frase ou uma palavra para verificar seéum palindromo ou não (Vamos lá): ");
+			String input = sc.nextLine();
+			
+			VerificatePalindrome verificate = new VerificatePalindrome(input);
+			
+			// as condições
+			
+			if(verificate.palindrome()) {
+				System.out.println("É um palíndromo\n");
+			}
+			else {
+				System.out.println("Não é um palíndromo\n");
+			}
 		
-		VerificatePalindrome verificate = new VerificatePalindrome(input);
-		
-		// as condições
-		
-		if(verificate.palindrome()) {
-			System.out.println("É um palíndromo\n");
+			sc.close();
+		}catch(Exception e){
+			System.out.println("Erro: " + e.getMessage());
+            e.printStackTrace();
 		}
-		else {
-			System.out.println("Não é um palíndromo\n");
-		}
-		
-		sc.close();
 		
 	}
 
